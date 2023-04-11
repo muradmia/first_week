2c63fead71dd823b9607432ea89cd7fa517fdec2
murad-mia <muradmia901@gmail.com>
2023-04-12 00:25:18 +0600

    week1_10-2-23

--- week1_10-2-23/problem1.c
+++ week1_10-2-23/problem1.c
@@ -4,7 +4,9 @@ int main(){
     char c;
     scanf("%c",&c);
 
-    if(c >='a' && c <= 'z'){
+    if(c == 'z'){
+        printf("%c",c-25);
+    }else if(c >='a' && c <= 'z'){
         printf("%c",c+1);
     }
 
